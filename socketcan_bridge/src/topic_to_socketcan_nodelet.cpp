fc1071a07a49c8eb442da08b9874032764e17601
Hongyu <hongyu.tian@enway.ai>
2020-04-07 10:31:59 +0800

    [socketcan_bridge] fix typo

--- socketcan_bridge/src/topic_to_socketcan_nodelet.cpp
+++ socketcan_bridge/src/topic_to_socketcan_nodelet.cpp
@@ -20,7 +20,7 @@ void socketcan_bridge::TopicToSocketcanNodelet::onInit()
   std::string can_device;
   nh.param<std::string>("can_device", can_device, "can0");
 
-  driver_ = boost::make_shared<can::ThreadedSocketCANInterface>s();
+  driver_ = boost::make_shared<can::ThreadedSocketCANInterface>();
   if (!driver_->init(can_device, 0))  // initialize device at can_device, 0 for no loopback.
   {
     NODELET_FATAL("Failed to initialize can_device at %s", can_device.c_str());
